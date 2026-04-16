42f8717f28f149cfb69e7ecbac426baa2dacfadc
xhq <1075012871@qq.com>
2026-04-16 14:49:21 +0800

    update

--- electron/main.js
+++ electron/main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow, Menu, ipcMain } = require('electron')
+const { app, BrowserWindow, Menu, ipcMain, globalShortcut } = require('electron')
 const { autoUpdater } = require('electron-updater')
 
 const fs = require('fs')
@@ -14,9 +14,11 @@ function createWindow() {
   // 开发模式加载 Vite 服务
   if (process.env.NODE_ENV === 'development') {
     win.loadURL('http://localhost:5173')
+    // win.webContents.openDevTools()
   } else {
     // 生产模式加载打包后的 index.html
     win.loadFile(path.join(__dirname, '../renderer/dist/index.html'))
+    // win.webContents.openDevTools()
   }
 
   // 菜单栏
@@ -49,7 +51,15 @@ autoUpdater.on('update-downloaded', () => {
 })
 
 ipcMain.handle('ping', async () => {
-  return 'pong from main process';
-});
+  return 'pong from main process'
+})
 
-app.whenReady().then(createWindow)
+app.whenReady().then(() => {
+  createWindow()
+  globalShortcut.register('Control+Shift+I', () => {
+    const focusedWindow = BrowserWindow.getFocusedWindow()
+    if (focusedWindow) {
+      focusedWindow.webContents.openDevTools()
+    }
+  })
+})
